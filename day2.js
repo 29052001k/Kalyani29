c2a81b471d5a77f956b19802433e103e9e8b3a3b
29052001k <kalyanihadole2001@gmail.com>
2023-02-05 20:05:58 +0530

    added

--- day2.js
+++ day2.js
@@ -1,1 +1,8 @@
 console.log("this is my second git class")
+
+
+console.log("Minskole class1")
+console.log("Minskole class2")
+console.log("Minskole class3")
+console.log("Minskole class4")
+console.log("Minskole class5")
